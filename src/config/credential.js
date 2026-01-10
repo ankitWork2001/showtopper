1f427d3169ff7e66a6812d102182a8851def41a8
Alok Singh <thealoksinghh@gmail.com>
2026-01-11 01:45:03 +0530

    minor modification

--- src/config/credential.js
+++ src/config/credential.js
@@ -1,5 +1,5 @@
 export const contactConfig = {
-  phoneNumber: "916747878723",
-  displayPhone: "+91 67478 78723",
+  phoneNumber: "9191674 78723",
+  displayPhone: "+91 91674 78723",
   whatsappMessage: "Hello Satyam Developers, I am interested in your property and would like to request a brief discussion at your convenience."
 };

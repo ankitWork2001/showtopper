ec5ce87e32a98a76c2ccf870dc1c149f2c5391dc
Alok Singh <thealoksinghh@gmail.com>
2026-01-16 20:35:05 +0530

    minor modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PublicLayout from "./layouts/PublicLayout";
 import AdminLayout from "./layouts/AdminLayout";
 
@@ -17,10 +17,13 @@ function App() {
           <Route path="/privacy" element={<PrivacyPage />} />
         </Route>
 
-        {/* 🔐 Admin Pages */}
+        {/* 🔐 Admin Pages
         <Route element={<AdminLayout />}>
           <Route path="/admin" element={<AdminPage />} />
-        </Route>
+        </Route> */}
+
+        {/* Catch all - redirect to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
 
       </Routes>
     </BrowserRouter>

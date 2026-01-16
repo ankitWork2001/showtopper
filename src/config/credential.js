e68e3679ff7b55528d6a164ce668b525e28e3978
Alok Singh <thealoksinghh@gmail.com>
2026-01-16 20:14:02 +0530

    fav icons added

--- src/config/credential.js
+++ src/config/credential.js
@@ -1,5 +1,5 @@
 export const contactConfig = {
-  phoneNumber: "916747878723",
+  phoneNumber: "9167478723",
   displayPhone: "+91 67478 78723",
   whatsappMessage:"Hello Satyam Developers, I am interested in your property and would like to request a brief discussion at your convenience.",
   propertyLocation: {
